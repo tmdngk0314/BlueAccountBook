9b452b2f7571cdc22949c9ac16f553aaf33c0c97
tmdngk0314 <tmdngk0314@naver.com>
2021-05-05 18:41:11 +0900

    달력기능 수정(제목 변경)

--- app/src/main/java/com/cookandroid/account_book/CalendarActivity.java
+++ app/src/main/java/com/cookandroid/account_book/CalendarActivity.java
@@ -43,7 +43,7 @@ public class CalendarActivity extends AppCompatActivity {
         Intent intent=getIntent();
         String name=intent.getStringExtra("userName");
         final String userID=intent.getStringExtra("userID");
-        textView3.setText(name+"님의 달력 일기장");
+
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override

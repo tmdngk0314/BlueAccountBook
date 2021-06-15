f2d8b4620f88fc337fb0223d090fbfdbc2957693
tmdngk0314 <tmdngk0314@naver.com>
2021-06-15 20:23:04 +0900

    주석

--- app/src/main/java/com/cookandroid/account_book/ListDBHelper.java
+++ app/src/main/java/com/cookandroid/account_book/ListDBHelper.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class ListDBHelper extends SQLiteOpenHelper {
+public class ListDBHelper extends SQLiteOpenHelper {   //내역에 쓰일 db helper
     public ListDBHelper(Context context){
         super(context,"groupDB",null,1);
     }
     @Override
     public void onCreate(SQLiteDatabase db){
         db.execSQL("CREATE TABLE groupTBL (gSuip INTEGER PRIMARY KEY,gJichull INTEGER, gJurchuck INTEGER);");
+        //테이블 생성
     }
 
     @Override
